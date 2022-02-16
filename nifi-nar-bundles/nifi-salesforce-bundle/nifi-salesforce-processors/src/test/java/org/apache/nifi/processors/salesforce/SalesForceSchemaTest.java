e98722901f9c9afd388bc58ac93a29ece75b3459
Lehel Boér <Lehel44@hotmail.com>
2022-02-16 18:20:44 +0100

    Salesforce RecordSchema converter added.

--- nifi-nar-bundles/nifi-salesforce-bundle/nifi-salesforce-processors/src/test/java/org/apache/nifi/processors/salesforce/SalesForceSchemaTest.java
+++ nifi-nar-bundles/nifi-salesforce-bundle/nifi-salesforce-processors/src/test/java/org/apache/nifi/processors/salesforce/SalesForceSchemaTest.java
@@ -15,19 +15,20 @@ import org.apache.avro.io.EncoderFactory;
 import org.apache.camel.component.salesforce.api.dto.SObjectDescription;
 import org.apache.camel.component.salesforce.api.dto.SObjectField;
 import org.apache.camel.component.salesforce.api.utils.JsonUtils;
+import org.apache.nifi.json.*;
+import org.apache.nifi.record.NullSuppression;
+import org.apache.nifi.schema.access.NopSchemaAccessWriter;
+import org.apache.nifi.schema.access.SchemaNotFoundException;
+import org.apache.nifi.serialization.*;
+import org.apache.nifi.serialization.record.*;
+import org.apache.nifi.util.MockComponentLog;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class SalesForceSchemaTest {
 
@@ -154,6 +155,7 @@ class SalesForceSchemaTest {
 
         }
 
+
         final Schema schema = fieldAssembler.endRecord();
 //        System.out.println(schema);
 //
@@ -168,6 +170,123 @@ class SalesForceSchemaTest {
         Decoder decoder1 = DecoderFactory.get().binaryDecoder(avroByteArray, null);
         GenericRecord result = reader1.read(null, decoder1);
 
+    }
+
+    @Test
+    void testSalesforceRecordSchema() throws IOException, SchemaNotFoundException, MalformedRecordException {
+        final MockComponentLog mockComponentLog = new MockComponentLog("logger", new Object());
+        final RecordSchema recordSchema = createSalesforceRecordSchema();
+        final String filePath = "src/test/resources/account/account_row.json";
+        final InputStream in = new FileInputStream(filePath);
+
+        JsonTreeRowRecordReader reader = new JsonTreeRowRecordReader(in, mockComponentLog, recordSchema, null, null, null);
+
+        final OutputStream out = new FileOutputStream("src/test/resources/account/result.json");
+        RecordSetWriter writer = new WriteJsonResult(mockComponentLog, recordSchema, new NopSchemaAccessWriter(), out, true,
+                NullSuppression.NEVER_SUPPRESS, OutputGrouping.OUTPUT_ONELINE, null, null, null);
+
+
+        //Record record;
+        //writer.beginRecordSet();
+        //while((record = reader.nextRecord()) != null) {
+        //    writer.write(record);
+        //}
+        //writer.finishRecordSet();
+
+        Record record = reader.nextRecord();
+
+        WriteResult writeResult = writer.write(RecordSet.of(record.getSchema(), record));
+
+        System.out.println(writeResult);
+
+        in.close();
+        out.close();
+    }
+
+    private RecordSchema createSalesforceRecordSchema() throws IOException {
+        final String describeAccount = readFile("src/test/resources/account/describe_account.json");
+        final ObjectMapper objectMapper = JsonUtils.createObjectMapper();
+        final SObjectDescription sObjectDescription = objectMapper.readValue(describeAccount, SObjectDescription.class);
+        final List<SObjectField> fields = sObjectDescription.getFields();
+
+        final List<RecordField> attributesFields = new ArrayList<>();
+        attributesFields.add(new RecordField("latitude", RecordFieldType.STRING.getDataType(), true));
+        attributesFields.add(new RecordField("longitude", RecordFieldType.STRING.getDataType(), true));
+        final RecordSchema attributesSchema = new SimpleRecordSchema(attributesFields);
+
+        final List<RecordField> recordFields = new ArrayList<>();
+        recordFields.add(new RecordField("attributes", RecordFieldType.RECORD.getRecordDataType(attributesSchema), false));
+
+        for (SObjectField field : fields) {
+            final String soapType = field.getSoapType();
+
+            switch (soapType.substring(soapType.indexOf(':') + 1)) {
+                case "ID": // mapping for tns:ID SOAP type
+                case "string":
+                case "base64Binary":
+                    // Salesforce maps any types like string, picklist, reference,
+                    // etc. to string
+                case "anyType":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.STRING.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "integer":
+                case "int":
+                case "long":
+                case "short":
+                case "byte":
+                case "unsignedInt":
+                case "unsignedShort":
+                case "unsignedByte":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.INT.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "decimal":
+                case "float":
+                case "double":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.DOUBLE.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "boolean":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.BOOLEAN.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "date":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.STRING.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "dateTime":
+                case "g":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.STRING.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "time":
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.TIME.getDataType(), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "address":
+                    final List<RecordField> addressFields = new ArrayList<>();
+                    addressFields.add(new RecordField("city", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("country", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("countryCode", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("postalCode", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("state", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("stateCode", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("street", RecordFieldType.STRING.getDataType(), true));
+                    addressFields.add(new RecordField("geocodeAccuracy", RecordFieldType.STRING.getDataType(), true));
+                    final RecordSchema addressSchema = new SimpleRecordSchema(addressFields);
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.RECORD.getRecordDataType(addressSchema), field.getDefaultValue(), field.isNillable()));
+                    break;
+                case "location":
+                    final List<RecordField> locationFields = new ArrayList<>();
+                    locationFields.add(new RecordField("latitude", RecordFieldType.STRING.getDataType(), true));
+                    locationFields.add(new RecordField("longitude", RecordFieldType.STRING.getDataType(), true));
+                    final RecordSchema locationSchema = new SimpleRecordSchema(locationFields);
+                    recordFields.add(new RecordField(field.getName(), RecordFieldType.RECORD.getRecordDataType(locationSchema), field.getDefaultValue(), field.isNillable()));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported type " + soapType);
+            }
+        }
+
+        return new SimpleRecordSchema(recordFields);
+    }
+
+    private Set<String> set(final String... values) {
+        return new HashSet<>(Arrays.asList(values));
     }
 
     private String readFile(final String path) throws IOException {
